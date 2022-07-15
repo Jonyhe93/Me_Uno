e8b378caa604508a197cf5d885ac1bcd4d273272
DESKTOP-14NS0AA\Jony <jonyhern93@gmail.com>
2022-07-15 21:27:23 +0200

    He generado los componentes dentro del app. añadido rutas en app routing y app module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { RegistrateComponent } from './registrate/registrate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "header", component: HeaderComponent, pathMatch: "full" },
+  { path: "footer", component: FooterComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "registrate", component: RegistrateComponent, pathMatch: "full" },
+  { path: "eventos", component: EventosComponent, pathMatch: "full" },
+  { path: "noticias", component: NoticiasComponent, pathMatch: "full" },
+  { path: "contacto", component: ContactoComponent, pathMatch: "full" }
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [AppComponent, LoginComponent, RegistrateComponent, HeaderComponent, FooterComponent, EventosComponent, ContactoComponent, NoticiasComponent],
+  imports: [BrowserModule, RouterModule],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './header/header.component';
+import { RegistrateComponent } from './registrate/registrate.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
+    HeaderComponent,
+    RegistrateComponent,
+    FooterComponent,
+    EventosComponent,
+    NoticiasComponent,
+    ContactoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
